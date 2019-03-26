1b434ffd61fe72d13db6797d3178e7990717997f
jostrek123 <46906572+jostrek123@users.noreply.github.com>
2019-03-26 13:53:33 -0500

    Create sms.js

---
+++ sms.js
@@ -0,0 +1,14 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Your Account Sid and Auth Token from twilio.com/console
+// DANGER! This is insecure. See http://twil.io/secure
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+18307150029',
+     to: '+19148060860'
+   })
+  .then(message => console.log(message.sid));
