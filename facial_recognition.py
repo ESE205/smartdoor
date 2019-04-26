901a5a36e4cbc447503b0bd2c0d58998e28e5ae7
ktcards <35540689+ktcards@users.noreply.github.com>
2019-04-25 21:20:25 -0500

    Update facial_recognition.py

--- facial_recognition.py
+++ facial_recognition.py
@@ -1,3 +1,4 @@
+import botocore
 import boto3
 import json
 import picamera
@@ -5,6 +6,18 @@ import time
 import requests
 import RPi.GPIO as GPIO
 
+import sys
+import os
+print(os.getcwd())
+print(sys.path)
+print(sys.executable)
+
+
+#copy access key here#
+#db= MySQLdb.connect(host="smarterdoornew.c0rgvoudpusn.us-east-2.rds.amazonaws.com", user="jostrek123", passwd="205pass!", db="smarterdoor")
+session = boto3.Session(aws_access_key_id= "AKIAIEVZX4YEFALN25MA",
+                        aws_secret_access_key="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",
+                        region_name='us-east-1')
 
 s3 = session.client('s3')
 updatedtime=time.time()
@@ -12,7 +25,6 @@ timestamp=str(round(updatedtime*1000))
 client=session.client('rekognition', 'us-east-1')
 bucket= 'smartdoorpictures'
 FACE_LIST= ["andrew", "johnny", "katie"]
-key_target= "intruder" +timestamp +".jpg"
 threshold=0
 RECOGNIZED_FACE = False
 IS_FACE=False
@@ -25,19 +37,124 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(17,GPIO.OUT)
 GPIO.setup(27,GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
+GPIO.setup(4, GPIO.IN)
+GPIO.setup(14,GPIO.OUT)
 
 while True:
-    input_state=GPIO.input(18)
-    if input_state == False:
+    i=GPIO.input(4)
+    print(GPIO.input(4))
+    #i=GPIO.input(7)
+    if i== 0:
+        print ("no motion detected")
+        time.sleep(1)
+    if i==1:
         #when camera is plugged in use this code
-        print ("button was pushed")
-        GPIO.output(17, True)
+        print ("motion detected")
+        key_target= "intruder" +timestamp +".jpg"
+        GPIO.output(22, True)
+        GPIO.output(27, True)
+        GPIO.output(14, True)
+        time.sleep(2)
+        GPIO.output(22, False)
+        GPIO.output(27, False)
+        GPIO.output(14, False)
         with picamera.PiCamera() as camera:
             camera.resolution= (1280, 720)
             camera.capture('intruderface.jpg')
-        GPIO.output(17, False)    
+        GPIO.output(27, False)    
         s3.upload_file('intruderface.jpg', Bucket=bucket, Key= key_target, ExtraArgs={'ContentType':"image/jpg"})
         
+        
+        
+        def detect_face(bucket, key, region_name = "us-east-1", attributes = ['ALL']):
+            rekognition = session.client("rekognition", region_name)
+            response = rekognition.detect_faces(
+                Image={
+                    "S3Object": {
+                        "Bucket": bucket,
+                        "Name": key
+                        }
+                    },
+                    Attributes = attributes,
+        )
+            return response['FaceDetails']
+            print(FaceDetails)
+
+        def compare_faces(bucket, key, bucket_target, key_target, threshold, region_name):
+            rekognition = session.client("rekognition", region_name)
+            response = rekognition.compare_faces(
+            SourceImage={
+            "S3Object": {
+                "Bucket": bucket,
+                "Name": key,
+                }
+                },
+                TargetImage={
+                "S3Object": {
+                "Bucket": bucket_target,
+                "Name": key_target,
+                }
+                },
+                SimilarityThreshold=threshold,
+                )
+            return response['SourceImageFace'], response['FaceMatches']
+            
+        
+        for face in detect_face(bucket, key_target):
+            conf = face['Confidence']
+            if(conf >= MIN_SIM):
+                message.append("face detected")
+                IS_FACE = True
+
+            #else:
+                #IS_FACE = False
+                #message.append("no face detected")
+                #rerun program
+        if (IS_FACE==True)  :
+            for face in FACE_LIST:
+                FILENAME= face + ".jpg"
+                source_face, matches = compare_faces(bucket, FILENAME, bucket, key_target, threshold, region_name)
+                for match in matches:
+                    sim = match['Similarity']
+                    message.append("simScore: " + str(sim))
+                    if (sim>= MIN_SIM) :
+                        message.append("user name: " + str(face))
+                        RECOGNIZED_FACE = True
+                        GPIO.output(22,True)
+                        time.sleep(20)
+                        GPIO.output(22, False)
+                        break
+                    
+        if (RECOGNIZED_FACE==False):
+                GPIO.output(27, True)
+                message.append("unknown user")
+                
+                
+        print(str.join(',', message))
+        print (key_target)
+        r=requests.get("http://3.208.217.100:1337/textuser/{}".format(key_target))
+            
+        while True:
+                if time.time()-updatedtime > 10:
+                    r=requests.get("http://3.208.217.100:1337/accessstatus/{}".format(key_target))
+                    response=json.loads(r.text)
+                    if response["status"]== "approved":
+                        GPIO.output(22, True)
+                        GPIO.output(27, False)
+                        time.sleep(20)
+                        GPIO.output(22, False)
+                        break
+                    if response["status"]=="denied":
+                        GPIO.output(27,False)
+                        GPIO.output(14, True)
+                        time.sleep(20)
+                        GPIO.output(14, False)
+                        break
+                    print (response["status"])
+                    updatedtime=time.time()
+        
+            
+
         
         
         def detect_face(bucket, key, region_name = "us-east-1", attributes = ['ALL']):
